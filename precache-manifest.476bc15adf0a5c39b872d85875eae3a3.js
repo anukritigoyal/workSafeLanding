50a0b7fde1c648363b030cc346bbc3c64db4bc43
anukritigoyal <anukriti@uw.edu>
2019-04-15 00:56:24 -0700

    Updates

---
+++ precache-manifest.476bc15adf0a5c39b872d85875eae3a3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad3ead1b83fe7427264d",
+    "url": "/workSafeLanding/static/css/main.8c354983.chunk.css"
+  },
+  {
+    "revision": "ad3ead1b83fe7427264d",
+    "url": "/workSafeLanding/static/js/main.56025e09.chunk.js"
+  },
+  {
+    "revision": "b3a5c114fcec5099d4cb",
+    "url": "/workSafeLanding/static/js/runtime~main.6bee288a.js"
+  },
+  {
+    "revision": "ab0dd78bb9773329408e",
+    "url": "/workSafeLanding/static/css/2.1f9f44f6.chunk.css"
+  },
+  {
+    "revision": "ab0dd78bb9773329408e",
+    "url": "/workSafeLanding/static/js/2.9c197bf1.chunk.js"
+  },
+  {
+    "revision": "f1cdf7dddfd83441c6e2add40dbd704c",
+    "url": "/workSafeLanding/static/media/first.f1cdf7dd.jpg"
+  },
+  {
+    "revision": "63e2b86e91f521712e6ba85cb217c44b",
+    "url": "/workSafeLanding/static/media/second.63e2b86e.jpg"
+  },
+  {
+    "revision": "b91db518aa28659f8d8f347d91c92b0c",
+    "url": "/workSafeLanding/static/media/ally.b91db518.jpg"
+  },
+  {
+    "revision": "d9e18a6e3102ed0af95a3f399ae1c469",
+    "url": "/workSafeLanding/static/media/anukriti.d9e18a6e.jpeg"
+  },
+  {
+    "revision": "7c56fb32a547a24647dd33269f7b78ed",
+    "url": "/workSafeLanding/static/media/sagar.7c56fb32.jpeg"
+  },
+  {
+    "revision": "f07b46128419a5e7549cc5fd3cde5959",
+    "url": "/workSafeLanding/static/media/luke.f07b4612.jpg"
+  },
+  {
+    "revision": "1b60b02f5a342f01b261db521c646b23",
+    "url": "/workSafeLanding/index.html"
+  }
+];
